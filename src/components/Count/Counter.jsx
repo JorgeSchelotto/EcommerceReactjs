c37c1ee2e4a1b81e327181b827294ca5861237bf
JorgeSchelotto <jasfotografo@hotmail.com>
2020-08-14 21:34:56 -0300

    Desafio contador con boton

---
+++ src/components/Count/Counter.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./style.css";
+
+
+const  Counter = ({initial, max, min}) => {
+    const [ count , setCount ] = useState(initial);
+    // count es el estado y setCount la funcion para actualizar el estado
+
+    function add(){
+      setCount( (!isNaN(parseFloat(count)) && isFinite(count)) ? (count < max ? count + 1: count): count )
+    };
+
+    function subtract(){
+      setCount( (!isNaN(parseFloat(count)) && isFinite(count)) ? (count > min ? count - 1: count): count )
+    };
+
+
+    return (
+      <>
+        <div className="countContainer">
+          <div className="countContainerAction">
+            <h3>{count}</h3>
+            <div className="countContainerActionButtons">
+              <button disabled={(count === max)} onClick={add} >+</button>
+              <button disabled={(count <= min)} onClick={subtract}>-</button>
+            </div>
+            <div className="countContainerCartButton">
+              <button
+                disabled={(count <= 0) || (count > max)}
+              > ADD</button>
+            </div>
+          </div>
+
+        </div>
+        
+      </>
+    )
+}
+
+export default Counter;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import Title from '../components/title'
-import Link from '../components/link'
+import Title from '../components/title';
+import Link from '../components/link';
+import Counter from "../components/Count/Counter";
 
 
 function Home({link, title, subtitle}){
 
     return (
         <>
-        <Title title={title} subtitle={subtitle} />
-        <Link link={link} />
+            <Title title={title} subtitle={subtitle} />
+            <Link link={link} />
+            <div>
+                <Counter
+                    initial={1}
+                    min={1}
+                    max={20}
+                />
+            </div>
+
+
         </>
 
     )
